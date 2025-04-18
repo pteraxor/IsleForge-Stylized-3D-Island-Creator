c0a51cdf32d022d53f4d8cb714450027e1211fcc
pteraxor <jdetznergaming@gmail.com>
2025-04-18 16:24:39 -0500

    cube viewer
    a cube can be viewed

--- IsleForge/Pages/TextureAndBumpPage.xaml.cs
+++ IsleForge/Pages/TextureAndBumpPage.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using HelixToolkit.Wpf.SharpDX;
+using SharpDX;
+using MediaColor = System.Windows.Media.Color;
 
 
 namespace IsleForge.Pages
@@ -25,7 +15,7 @@ namespace IsleForge.Pages
     public partial class TextureAndBumpPage : Page
     {
 
-    
+
         public TextureAndBumpPage()
         {
             InitializeComponent();
@@ -34,6 +24,40 @@ namespace IsleForge.Pages
 
         private void TextureAndBumpPage_Loaded(object sender, RoutedEventArgs e)
         {
+
+            Viewport.EffectsManager = new DefaultEffectsManager();
+            // 1. Build the cube geometry
+            var builder = new MeshBuilder();
+            builder.AddBox(new Vector3(0, 0, 0), 1f, 1f, 1f);
+            var mesh = builder.ToMeshGeometry3D();
+
+            // 2. Load normal map from resources
+            TextureModel normalMap = null;
+            var normalMapPath = "Resources/Textures/TestNormal.png";
+            if (File.Exists(normalMapPath))
+            {
+                using var stream = File.OpenRead(normalMapPath);
+                normalMap = new TextureModel(stream);
+            }
+
+            // 3. Define material
+            var material = new PhongMaterial
+            {
+                DiffuseColor = MediaColor.FromRgb(128, 128, 128).ToColor4(),
+                AmbientColor = MediaColor.FromRgb(64, 64, 64).ToColor4(),
+                SpecularColor = Color.White,
+                SpecularShininess = 100f,
+                NormalMap = normalMap
+            };
+
+            // 4. Create the model and add to viewport
+            var model = new MeshGeometryModel3D
+            {
+                Geometry = mesh,
+                Material = material
+            };
+
+            Viewport.Items.Add(model);
         }
 
     }
