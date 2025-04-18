30aa8bae601529bdeff6a961272b450aa3f8cfc0
pteraxor <jdetznergaming@gmail.com>
2025-04-18 15:42:44 -0500

    clean texture and bump
    these have been giving me a lot of trouble, need to just back up the clean versions.

---
+++ IsleForge/Pages/TextureAndBumpPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace IsleForge.Pages
+{
+    /// <summary>
+    /// Interaction logic for TextureAndBumpPage.xaml
+    /// </summary>
+    public partial class TextureAndBumpPage : Page
+    {
+
+    
+        public TextureAndBumpPage()
+        {
+            InitializeComponent();
+            this.Loaded += TextureAndBumpPage_Loaded;
+        }
+
+        private void TextureAndBumpPage_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+    }
+}
